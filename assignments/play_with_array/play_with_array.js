569923527052b2414e50403d55baf2a8a2c4b656
Thrivikram <radhakrishna@radhakrishna-ThinkPad-X230>
2022-03-03 12:01:42 +0530

    completed play with array

--- assignments/play_with_array/play_with_array.js
+++ assignments/play_with_array/play_with_array.js
@@ -1,35 +1,22 @@
 function getEven(arr) {
-    /* 
-        Return an array containing only even numbers as a result
-        Example
-            Input: [1,4,5,77,8,90]
-            Output: [4,8,90]
-
-        Write you code below
-    */
-
-}
-
+    let array = [...arr];
+    array.map((user) => {
+        if(array[user] % 2 !== 0){
+           array.splice(user,1);
+        }
+    })
+return array;
+};
 function multiplyByN(arr, n) {
-    /* 
-        Return an array whose elements are multiplied by n
-        Example
-            Input: [1,3,4,55], 3
-            Output: [3,9,13,165]
-        Write you code below
-    */
+   for(let i = 0; i < arr.length;i++){
+       arr[i] *= n;
+   };
+return arr;
 }
-
 function removeNthElement(arr, n) {
-    /*
-        Return the array with the element present at index n removed
-        Example
-            Input: [1,3,4,6,7] 3
-            Output: [1,3,4,7]
-        Write you code below
-    */
+    arr.splice(n,1);
+    return arr;
 }
-
 module.exports = {
     getEven,
     multiplyByN,
