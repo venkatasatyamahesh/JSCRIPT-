88236c5bae80bdba16f5fe1ea2ff8581320414a5
Thrivikram <radhakrishna@radhakrishna-ThinkPad-X230>
2022-03-03 11:08:16 +0530

    changes were made and assignment is completed

--- assignments/bmi_calculator/bmi_calculator.js
+++ assignments/bmi_calculator/bmi_calculator.js
@@ -4,8 +4,10 @@ function BMICalculator(mass, height) {
     let heightOfPerson = height;
     if(massOfPerson !== 0){
         if(heightOfPerson !== 0){
-    let BMI =  massOfPerson / (heightOfPerson * heightOfPerson)
-return BMI}};
+            if (/d+/gi.test(massOfPerson) ){
+                if (/d+/gi.test(heightOfPerson)){
+                     let BMI =  massOfPerson / (heightOfPerson * heightOfPerson);
+                         return BMI}}}};
 return "INVALID INPUT";
 
 }
