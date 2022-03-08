46e3e6113953f9d844d397364c787ddf383598ba
venkatasatyamahesh <98881109+venkatasatyamahesh@users.noreply.github.com>
2022-03-08 22:59:24 +0530

    PlayWithArray

--- assignments/play_with_array/play_with_array.js
+++ assignments/play_with_array/play_with_array.js
@@ -6,10 +6,12 @@ function getEven(arr) {
             Output: [4,8,90]
 
         Write you code below
-    */
+        function getEven(arr) {        var result = arr.filter(numb => numb%2===0)    return result}
+    */var result =arr.filter(numb => numb%2===0)
+
+        return result
 
 }
-
 function multiplyByN(arr, n) {
     /* 
         Return an array whose elements are multiplied by n
@@ -17,21 +19,29 @@ function multiplyByN(arr, n) {
             Input: [1,3,4,55], 3
             Output: [3,9,13,165]
         Write you code below
-    */
+  */function mulByN(num){
+      return num*n
+  }
+  arr=arr.map(mulByN)
+  return arr
 }
 
 function removeNthElement(arr, n) {
-    /*
-        Return the array with the element present at index n removed
-        Example
-            Input: [1,3,4,6,7] 3
-            Output: [1,3,4,7]
+        /*
+    
+            Return the array with the element present at index n removed
+                Example
+                    Input: [1,3,4,6,7] 3                                    Output: [1,3,4,7]
         Write you code below
-    */
-}
-
-module.exports = {
-    getEven,
-    multiplyByN,
-    removeNthElement
-}
+            */const opp = arr.filter((element,index) => {
+                return index !==n;
+            });
+            return opp
+    }
+    
+    module.exports = {
+        getEven,
+        multiplyByN,
+        removeNthElement
+    }
+    //function mul_by_N(num)
